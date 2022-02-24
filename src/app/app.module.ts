becabf5d3d42339a0e0bdbd1e48ad6b0fa7c5b02
giovani <giovani_floreal2012@homail.com>
2022-02-24 18:15:32 -0300

    tela inicial em andamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { CarrerComponent } from './carrer/carrer.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { AboutComponent } from './about/about.component';
+import { PlaygroundComponent } from './playground/playground.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    CarrerComponent,
+    JobsComponent,
+    AboutComponent,
+    PlaygroundComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
